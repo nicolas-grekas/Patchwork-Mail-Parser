6fbc8574cbbf079edfd6e4fb529f117dc4c65da5
sebastien.lavallee <lavallee.sebastien@gmail.com>
2011-06-01 18:23:47 +0200

    +handle default content-Type & allow plugin use cT

--- Stream/Parser/Mail.php
+++ Stream/Parser/Mail.php
@@ -2,7 +2,6 @@
 
 // TODO:
 // - detect and warn for malformed messages
-// - expose nested content-type structure
 // - parse headers
 // - Content-Transfert-Encoding
 
@@ -22,12 +21,23 @@ class Stream_Parser_Mail extends Stream_Parser
     $envelopeClientHelo,
     $envelopeClientHostname,
 
-    $mimePart = false,
-    $contentType = '',
+    $mimePart = array(
+        'parent'   => false,
+        'depth'    => 0,
+        'index'    => 0,
+        'boundary' => false,
+        'contentType' => 'message/rfc822',
+        'boundarySelector' => array(),
+        'defaultContentType' => false,
+    ),
+
+    $contentType = 'message/rfc822',
+    $nextContentType = 'text/plain',
 
     $callbacks = array(
-        'tagMailHeader'      => T_STREAM_LINE,
-        'extractContentType' => array(T_MAIL_HEADER => '/^Content-Type:\s+(.*)/si'),
+        'tagMailHeader'       => T_STREAM_LINE,
+        'extractContentType'  => array(T_MAIL_HEADER => '/^Content-Type:\s+(.*)/si'),
+        'registerContentType' => T_MAIL_BOUNDARY,
     );
 
 
@@ -39,12 +49,14 @@ class Stream_Parser_Mail extends Stream_Parser
         $this->envelopeClientHelo     = $helo;
         $this->envelopeClientHostname = $hostname;
 
+        $this->mimePart = (object) $this->mimePart;
+
         parent::__construct($parent);
     }
 
-    protected function tagMailHeader(&$line)
+    protected function tagMailHeader(&$line,$m, $tags)
     {
-        if (isset($tags[T_MIME_BOUNDARY])) return T_MAIL_BOUNDARY;
+        if (isset($tags[T_MIME_BOUNDARY])) return;
 
         static $nextHeader = array();
 
@@ -69,41 +81,76 @@ class Stream_Parser_Mail extends Stream_Parser
 
     protected function extractContentType($line, $matches)
     {
-        $this->contentType = $matches[1];
+        $this->nextContentType = $matches[1];
     }
 
     protected function tagMailBoundary($line)
     {
         $this->unregister(array(__FUNCTION__ => T_STREAM_LINE));
+        $this->register(array('tagMailBody' => T_STREAM_LINE));
 
-        switch (true)
-        {
-        case preg_match('/^multipart\/.*;\s*boundary=("?)(.*)\1/si', $this->contentType, $m):
-            $s = array(T_STREAM_LINE => '/^--(' . preg_quote($m[2], '/') . ')(--)?/');
-            $this->mimePart = (object) array(
-                'parent'   => $this->mimePart,
-                'depth'    => $this->mimePart ? $this->mimePart->depth + 1 : 1,
-                'index'    => 0,
-                'boundary' => $m[2],
-                'boundarySelector' => $s,
-            );
+        $this->contentType = $this->nextContentType;
+        $this->nextContentType = false;
 
-            $this->register(array('tagMimeBoundary' => $s));
-            $this->register(array('tagMimeIgnore' => T_STREAM_LINE));
-            break;
-
-        case preg_match('/^(message\/rfc822|text\/rfc822-headers)/', $this->contentType):
-            $this->register(array('tagMailHeader' => T_STREAM_LINE));
-            break;
-
-        default:
-            $this->register(array('tagMailBody' => T_STREAM_LINE));
-            break;
-        }
-
-        $this->contentType = '';
+        if (false === $this->mimePart->contentType)
+            $this->mimePart->contentType = $this->contentType;
 
         return T_MAIL_BOUNDARY;
+    }
+
+    protected function registerContentType()
+    {
+        switch (true)
+        {
+        case preg_match('/^(message\/|text\/rfc822-headers)/', $this->contentType):
+            $this->registerRfc822Part();
+            break;
+
+        case preg_match('/^multipart\/(.*?);.*boundary=("?)(.*)\2/si', $this->contentType, $m):
+            $this->registerMimePart($m[3], strcasecmp('digest', $m[1]) ? 'text/plain' : 'message/rfc822');
+            break;
+        }
+    }
+
+    protected function registerRfc822Part($nextContentType = 'text/plain')
+    {
+        if (false !== $this->nextContentType)
+        {
+            $this->setError("Failed to set Mail->nextContentType to `{$nextContentType}`: already set to `{$this->nextContentType}`", E_USER_WARNING);
+            return;
+        }
+
+        $this->unregister(array('tagMailBody' => T_STREAM_LINE));
+        $this->register(array('tagMailHeader' => T_STREAM_LINE));
+
+        $this->nextContentType = $nextContentType;
+    }
+
+    protected function registerMimePart($boundary, $defaultContentType)
+    {
+        if (false !== $this->nextContentType)
+        {
+            $this->setError("Failed to set Mail->nextContentType to `{$nextContentType}`: already set to `{$this->nextContentType}`", E_USER_WARNING);
+            return;
+        }
+
+        $this->unregister(array('tagMailBody' => T_STREAM_LINE));
+
+        $s = array(T_STREAM_LINE => '/^--(' . preg_quote($boundary, '/') . ')(--)?/');
+        $this->mimePart = (object) array(
+            'parent'   => $this->mimePart,
+            'depth'    => $this->mimePart->depth + 1,
+            'index'    => 0,
+            'boundary' => $boundary,
+            'contentType' => false,
+            'boundarySelector' => $s,
+            'defaultContentType' => $defaultContentType,
+        );
+
+        $this->register(array('tagMimeBoundary' => $s));
+        $this->register(array('tagMimeIgnore' => T_STREAM_LINE));
+
+        $this->nextContentType = $defaultContentType;
     }
 
     protected function tagMimeBoundary($line, $matches)
@@ -125,6 +172,8 @@ class Stream_Parser_Mail extends Stream_Parser
         if (empty($matches[2]))
         {
             ++$p->index;
+            $p->contentType = false;
+            $this->nextContentType = $p->defaultContentType;
             $this->register(array('tagMailHeader' => T_STREAM_LINE));
         }
         else
