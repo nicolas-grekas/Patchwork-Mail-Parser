047f380dd99b2b6e76cf6a42fa980383a19da020
sebastien.lavallee <lavallee.sebastien@gmail.com>
2011-08-22 17:52:49 +0200

    new parser-mail coresponding with the new plug-ins

--- parse-mail-stdin.php
+++ parse-mail-stdin.php
@@ -12,23 +12,34 @@ require __DIR__ . '/Stream/Parser/Mail/Bounce.php';
 require __DIR__ . '/Stream/Parser/Mail/EnvelopeHeaders.php';
 require __DIR__ . '/Stream/Parser/Mail/Auth/Received.php';
 require __DIR__ . '/Stream/Parser/Mail/Auth/Greylist.php';
+require __DIR__ . '/Stream/Parser/Mail/Auth/MessageId.php';
 require __DIR__ . '/Stream/Parser/Mail/Bounce/Rfc3464.php';
 require __DIR__ . '/Stream/Parser/Mail/Bounce/Autoreply.php';
 require __DIR__ . '/Stream/Parser/Mail/Bounce/Qmail.php';
+require __DIR__ . '/Stream/Parser/Mail/Bounce/Exim.php';
+require __DIR__ . '/Stream/Parser/Mail/Bounce/ReceivedFor.php';
+
+require __DIR__ . '/BouncePdoAdapter.php';
 
 $local_whitelist = array(
 );
 
+$db = new PDO('mysql:host=localhost;dbname=bounces', 'root', 'hp');
+$db = new BouncePdoAdapter($db);
+
 $parser = new Stream_Parser;
-new Stream_Parser_Mail($parser);
+$mail = new Stream_Parser_Mail($parser);
 new Stream_Parser_Mail_EnvelopeHeaders($parser);
 $auth = new Stream_Parser_Mail_Auth($parser);
 new Stream_Parser_Mail_Auth_Received($parser, $local_whitelist);
 new Stream_Parser_Mail_Auth_Greylist($parser);
+new Stream_Parser_Mail_Auth_MessageId($parser, array($db, 'countMessageId'));
 $boun = new Stream_Parser_Mail_Bounce($parser);
 new Stream_Parser_Mail_Bounce_Rfc3464($parser);
 new Stream_Parser_Mail_Bounce_Autoreply($parser);
 new Stream_Parser_Mail_Bounce_Qmail($parser);
+new Stream_Parser_Mail_Bounce_Exim($parser);
+new Stream_Parser_Mail_Bounce_ReceivedFor($parser);
 
 //new Stream_Parser_Log($parser);
 
@@ -40,9 +51,13 @@ if ($e = $parser->getErrors())
 }
 else
 {
+    $mail = $mail->getEnvelope();
     $auth = $auth->getAuthenticationResults();
     $boun = $boun->getBounceReports();
 
+    $auth['sent-time'] = $db->getAuthSentTime($mail->recipient, $boun);
+
+    print_r($mail);
     print_r($auth);
     print_r($boun);
 }
